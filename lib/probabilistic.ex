cc8ec7bf59a4fa12e31a5a2cefc5dae0f50b6733
preciz <barna.kovacs.codes@gmail.com>
2019-09-25 21:39:30 +0200

    Remove boilerplate code

--- lib/probabilistic.ex
+++ lib/probabilistic.ex
@@ -2,17 +2,4 @@ defmodule Probabilistic do
   @moduledoc """
   Documentation for Probabilistic.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Probabilistic.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
