ac858c8234a6a04a7f091ad1d3ddc734dd8822f2
preciz <barna.kovacs.codes@gmail.com>
2019-09-26 00:32:11 +0200

    [WIP] stream

---
+++ lib/probabilistic/stream.ex
@@ -0,0 +1,30 @@
+defmodule Probabilistic.Stream do
+  alias Probabilistic.BloomFilter
+
+  @doc """
+  Returns a probabilisticly uniq stream.
+
+  Its main advantage is that it doesn't store elements
+  emitted by the stream.
+
+  The stream never returns duplicate elements but it
+  sometimes detects false positive duplicates depending on the
+  bloom filter it uses.
+  """
+  def uniq(enum) do
+    bloom_filter = BloomFilter.new(1_000_000)
+
+    uniq(enum, bloom_filter)
+  end
+
+  def uniq(enum, bloom_filter) do
+    enum
+    |> Stream.reject(fn x ->
+      is_member = bloom_filter |> BloomFilter.member?(x)
+
+      bloom_filter |> BloomFilter.put(x)
+
+      is_member
+    end)
+  end
+end
