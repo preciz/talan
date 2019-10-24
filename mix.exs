28467f491ca5a885b6368b1f7c466bbffbfe739b
preciz <barna.kovacs.codes@gmail.com>
2019-10-24 21:16:40 +0200

    Prepare package for release on hex.pm

--- mix.exs
+++ mix.exs
@@ -1,24 +1,53 @@
 defmodule Talan.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @github "https://github.com/preciz/talan"
+
   def project do
     [
       app: :talan,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      package: package(),
+      homepage_url: @github,
+      description: """
+      Probabilistic data structures powered by atomics.
+      Bloom filter, Counting bloom filter, Linear counter (cardinality)
+      """
     ]
   end
 
   def application do
-    []
+    [
+      extra_applications: []
+    ]
   end
 
   defp deps do
     [
       {:murmur, "~> 1.0"},
       {:abit, "~> 0.3"},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
+    ]
+  end
+
+  defp docs do
+    [
+      main: "Talan",
+      source_ref: "v#{@version}",
+      source_url: @github,
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Barna Kovacs"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @github}
     ]
   end
 end
